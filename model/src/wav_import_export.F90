da6f509e3d01cf4bb7534db3baa5d7a3ac08fde3
Mariana Vertenstein <marianav@met.no>
2023-12-04 19:28:29 +0100

    changes for new swell cmeps auxiliary file

--- model/src/wav_import_export.F90
+++ model/src/wav_import_export.F90
@@ -101,17 +101,17 @@ contains
   !!
   !> @author mvertens@ucar.edu, Denise.Worthen@noaa.gov
   !> @date 01-05-2022
-  subroutine advertise_fields(importState, ExportState, flds_scalar_name, rc)
+  subroutine advertise_fields(importState, ExportState, flds_scalar_name, aux_flds_to_cmeps, rc)
     ! input/output variables
     type(ESMF_State)               :: importState
     type(ESMF_State)               :: exportState
+    logical          , intent(in)  :: aux_flds_to_cmeps
     character(len=*) , intent(in)  :: flds_scalar_name
     integer          , intent(out) :: rc
 
     ! local variables
     integer          :: n, num
     character(len=2) :: fvalue
-    logical          :: aux_flds_to_cmeps = .true.
     character(len=*), parameter :: subname='(wav_import_export:advertise_fields)'
     !-------------------------------------------------------------------------------
 
@@ -122,7 +122,7 @@ contains
     ! Advertise import fields
     !--------------------------------
 
-    !call fldlist_add(fldsToWav_num, fldsToWav, 'So_h'       )
+   !call fldlist_add(fldsToWav_num, fldsToWav, 'So_h'       )
     call fldlist_add(fldsToWav_num, fldsToWav, 'Si_ifrac'   )
     call fldlist_add(fldsToWav_num, fldsToWav, 'So_u'       )
     call fldlist_add(fldsToWav_num, fldsToWav, 'So_v'       )
